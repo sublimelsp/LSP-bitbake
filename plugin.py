b17d7d752f81aecbddb5f10ced01fd5fc301b8ac
Rafał Chłodnicki <rchl2k@gmail.com>
2026-05-29 21:28:40 +0200

    refactor: migrate to LspPlugin (#3)

--- plugin.py
+++ plugin.py
@@ -1,20 +1,32 @@
 from __future__ import annotations
 
-from lsp_utils import NpmClientHandler
-import os
+from LSP.plugin import LspPlugin
+from LSP.plugin import OnPreStartContext
+from lsp_utils import NodeManager
+from pathlib import Path
+from sublime_lib import ResourcePath
+from typing_extensions import override
 
 
 def plugin_loaded():
-    LspBitbakefilePlugin.setup()
+    LspBitbakefilePlugin.register()
 
 
 def plugin_unloaded():
-    LspBitbakefilePlugin.cleanup()
+    LspBitbakefilePlugin.unregister()
 
 
-class LspBitbakefilePlugin(NpmClientHandler):
-    package_name = str(__package__)
-    server_directory = "server"
-    server_binary_path = os.path.join(
-        server_directory, "node_modules", "language-server-bitbake", "out", "server.js"
-    )
+class LspBitbakefilePlugin(LspPlugin):
+
+    @classmethod
+    @override
+    def on_pre_start_async(cls, context: OnPreStartContext) -> None:
+        package_name = cls.plugin_storage_path.name
+        NodeManager.on_pre_start_async(
+            context,
+            cls.plugin_storage_path,
+            ResourcePath('Packages', package_name, 'server'),
+            Path('node_modules', 'language-server-bitbake', 'out', 'server.js'),
+            node_version_requirement='>=18',
+        )
+
